55f9bfb8bd2bc1cbcb714401a2e107be03bf8120
maexftw <maexftw@gmail.com>
2026-03-11 17:25:39 +0100

    chore: initial astro conversion with tinacms setup

---
+++ .tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "ad3dd020-15e1-4cab-8913-6148dbc5a406", // Get this from tina.io
+  token: "3f393f890c7382ad9f951770bfbfa5c89afd24e1", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "posts",
+        label: "Posts",
+        path: "src/content/posts",
+        format: 'md',
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
